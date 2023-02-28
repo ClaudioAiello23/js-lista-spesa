c9016839fbfa1f5221414aca77b5e4a6ff218e34
ClaudioAiello23 <claudio.aiello23@gmail.com>
2023-02-28 15:43:00 +0100

    inserimento lista in Dom (html) - da correggere

--- js/main.js
+++ js/main.js
@@ -1,5 +1,53 @@
 'use strict';
 
-alert('ciao');
+const myShoppingList = [
+    'Nutella',
+    'Pan grattato',
+    'Pizza Buitoni',
+    'Burro', 
+    'Sacchetti umido',
+    'Vino Primitivo di Manduria',
+    'Birra Heineken', 
+    'KitKat', 
+    'Biscotti Krumiri', 
+    'Focaccine', 
+    'Detersivo piatti', 
+    'Sugo Mutti', 
+    'Gelati Sammontana', 
+    'Cuori di pane Mulino Bianco', 
+    'Maionese', 
+    'Penne Barilla', 
+    'TicTac colored', 
+    'Vasetto miele', 
+    'Acqua Ferrarelle',
+    'Acqua Levissima',
+    'Uovo Pasqua Kinder',
+    'Cartucce stampante Epson',
+    'Figurine Calciatori Panini'
+];
+
+
+
+  let i = 0;
+  console.log('LA MIA LISTA DELLA SPESA:');
+
+  const finalList = document.getElementById('shopping_list');
+
+  while (i < myShoppingList.length){
+    
+      console.log(i);
+      i++;
+      console.log(myShoppingList[i]);
+
+      const listItems = document.createElement('li');
+      listItems.append(myShoppingList[i]);
+      finalList.append(listItems);
+
+  }
+
+
+
+
+
 
 
